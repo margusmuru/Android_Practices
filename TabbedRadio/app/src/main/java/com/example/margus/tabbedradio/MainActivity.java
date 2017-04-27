d41055eea015ebede5d08210a131c1e21878b0de
Margus Muru <margusmuru@outlook.com>
2017-04-27 14:45:58 +0300

    TabbedRadio prax1

---
+++ TabbedRadio/app/src/main/java/com/example/margus/tabbedradio/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.margus.tabbedradio;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar mToolBar;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+
+        //initialize components
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolBar);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(mViewPager);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tablayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new FragmentOne(), "One");
+        adapter.addFragment(new FragmentTwo(), "Two");
+        adapter.addFragment(new FragmentThree(), "Three");
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPageChangeListener(adapter));
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mTitleList.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mTitleList.get(position);
+        }
+    }
+
+    class ViewPageChangeListener extends ViewPager.SimpleOnPageChangeListener{
+
+        ViewPagerAdapter mAdapter;
+
+        public ViewPageChangeListener(ViewPagerAdapter adapter){
+            mAdapter = adapter;
+        }
+
+        @Override
+        public void onPageSelected(int position){
+            Log.d(TAG, "Tab changed: " + mAdapter.getPageTitle(position));
+        }
+    }
+
+}
